ea22357851710fb08fb9bb316b6cf528037780a4
Rodrigo Yanez <wintermute.ryf@gmail.com>
2023-06-22 07:31:06 -0600

    v2 updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,69 @@
+const url = "https://restcountries.com/v3.1/name/";
 const searchBtn = document.getElementById("search-btn");
 const countryInp = document.getElementById("country-inp");
-searchBtn.addEventListener("click", () => {
+const displayResult = document.getElementById("result");
+searchBtn.addEventListener("click", getInfo);
+async function getInfo() {
   const countryName = countryInp.value;
-  const finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-  console.log(finalURL);
-  fetch(finalURL)
-    .then((response) => response.json())
-    .then((data) => {
-         console.log(data[0]);
-      //   console.log(data[0].capital[0]);
-      //   console.log(data[0].flags.svg);
-      //   console.log(data[0].name.common);
-      //   console.log(data[0].continents[0]);
-      //   console.log(Object.keys(data[0].currencies)[0]);
-      //   console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-      //   console.log(
-      //     Object.values(data[0].languages).toString().split(",").join(", ")
-      //   );
-      result.innerHTML = `
-        <img src="${data[0].flags.svg}" class="flag-img">
-        <h2>${data[0].name.common}</h2>
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Capital:</h4>
-                <span>${data[0].capital[0]}</span>
-            </div>
+  const endpoint = `${url}${countryName}`;
+  console.log(endpoint);
+  if (countryName.length === 0) {
+    displayResult.innerHTML = "<h3>The input field cannot be empty</h3>";
+    return;
+  }
+
+  try {
+    const response = await fetch(endpoint, { cache: "no-cache" });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+
+      showResult(jsonResponse);
+    } else {
+      displayResult.innerHTML = "<h3>Please enter a valid country name.</h3>";
+    }
+  } catch (error) {
+    displayResult.innerHTML =
+      "<h3>An error occurred. Please try again later.</h3>";
+  }
+}
+function showResult(data) {
+  displayResult.innerHTML = `
+    <img src="${data[0].flags.svg}" class="flag-img">
+  <h2>${data[0].name.common}</h2>
+  <div class="wrapper">
+      <div class="data-wrapper">
+          <h4>Capital:</h4>
+          <span>${data[0].capital[0]}</span>
+      </div>
+  </div>
+  <div class="wrapper">
+      <div class="data-wrapper">
+          <h4>Continent:</h4>
+          <span>${data[0].continents[0]}</span>
+      </div>
+  </div>
+   <div class="wrapper">
+      <div class="data-wrapper">
+          <h4>Population:</h4>
+          <span>${data[0].population}</span>
+      </div>
+  </div>
+  <div class="wrapper">
+      <div class="data-wrapper">
+          <h4>Currency:</h4>
+          <span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${
+    Object.keys(data[0].currencies)[0]
+  }</span>
         </div>
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Continent:</h4>
-                <span>${data[0].continents[0]}</span>
-            </div>
+    </div>
+     <div class="wrapper">
+        <div class="data-wrapper">
+            <h4>Common Languages:</h4>
+            <span>${Object.values(data[0].languages)
+              .toString()
+              .split(",")
+              .join(", ")}</span>
         </div>
-         <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Population:</h4>
-                <span>${data[0].population}</span>
-            </div>
-        </div>
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Currency:</h4>
-                <span>${
-                  data[0].currencies[Object.keys(data[0].currencies)].name
-                } - ${Object.keys(data[0].currencies)[0]}</span>
-            </div>
-        </div>
-         <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Common Languages:</h4>
-                <span>${Object.values(data[0].languages)
-                  .toString()
-                  .split(",")
-                  .join(", ")}</span>
-            </div>
-        </div>
-      `;
-    })
-    .catch(() => {
-      if (countryName.length === 0) {
-        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
-      } else {
-        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
-      }
-    });
-});
+    </div>
+  `;
+}
